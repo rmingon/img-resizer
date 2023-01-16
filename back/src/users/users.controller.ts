b750f57cffd996363abf934a1543e7f3cb9c5df0
rmingon <ronanmingon69550@gmail.com>
2023-01-16 07:05:44 +0100

    add user controller

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User, Prisma } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(
+    @Body() userDto: Omit<User, 'id'>,
+  ): Promise<Omit<User, 'password'>> {
+    const { password, ...rest } = await this.usersService.create(userDto);
+    return rest;
+  }
+
+}
