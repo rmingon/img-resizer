92a8c42fa8a198aa39cf269986f536d40a100d2e
rmingon <ronanmingon69550@gmail.com>
2023-01-16 07:05:21 +0100

    add user service

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  create(userDto: Omit<User, 'id'>): Promise<User> {
+    userDto.password = bcrypt.hashSync(userDto.password, process.env.PASS_HASH);
+    return this.prisma.user.create({
+      data: userDto,
+    });
+  }
+}
