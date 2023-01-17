259f15bed10d7f900fda4d28830a3742e53060dd
rmingon <ronanmingon69550@gmail.com>
2023-01-17 06:49:24 +0100

    fix user create

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  create(userDto: Omit<User, 'id'>): Promise<User> {
-    userDto.password = bcrypt.hashSync(userDto.password, process.env.PASS_HASH);
+  async create(userDto: Omit<User, 'id'>): Promise<User> {
+    userDto.password = await bcrypt.hash(userDto.password, 10);
     return this.prisma.user.create({
       data: userDto,
     });
