31e407311d7da1a3abaa9d165f5b79788e157692
rmingon <ronanmingon69550@gmail.com>
2023-01-18 06:55:00 +0100

    add navbar

---
+++ front/components/organisms/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-between">
+    <div> <!-- LOGO -->
+
+    </div>
+    <div class="flex text-white">
+
+      <topbar-link label="TOTO" />
+      <topbar-link label="TOTO" />
+      <topbar-link label="TOTO" />
+      <topbar-link label="TOTO" />
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import TopbarLink from "~/components/molecules/TopbarLink.vue";
+
+
+</script>
+
+<style scoped>
+
+</style>
