2e74a452d7421444a27dda99aaa514286e7a22d5
rmingon <ronanmingon69550@gmail.com>
2023-01-18 06:55:11 +0100

    add topbarlink

---
+++ front/components/molecules/TopbarLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text-white">
+    {{ props.label }}
+  </p>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    label: string,
+  }>()
+</script>
+
+<style scoped>
+
+</style>
