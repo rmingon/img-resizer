26454c49ee8d42950b006fc2e7e7a277b0275951
rmingon <ronanmingon69550@gmail.com>
2023-01-20 08:03:37 +0100

    add topbar + img

--- front/app.vue
+++ front/app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="h-screen w-full bg-midnight">
+    <the-navbar class="z-10"></the-navbar>
+    <img src="@/assets/img/boat-beach.jpg" alt="" class="absolute top-0 w-1/5 rounded ">
   </div>
 </template>
+<script setup lang="ts">
+import TheNavbar from "~/components/organisms/TheNavbar.vue";
+
+</script>
