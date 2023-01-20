511b40c2d4adf225b500947f23f6f18fbe309198
rmingon <ronanmingon69550@gmail.com>
2023-01-20 08:02:44 +0100

    edit navbar

--- front/components/organisms/TheNavbar.vue
+++ front/components/organisms/TheNavbar.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="flex flex-between">
+  <div class="flex flex-between items-center py-3 w-full">
     <div> <!-- LOGO -->
 
     </div>
-    <div class="flex text-white">
-
-      <topbar-link label="TOTO" />
-      <topbar-link label="TOTO" />
-      <topbar-link label="TOTO" />
-      <topbar-link label="TOTO" />
+    <div class="flex text-white space-x-2">
+      <topbar-link label="TOTO"/>
+      <topbar-link label="TOTO"/>
+      <topbar-link label="TOTO"/>
+      <topbar-link label="TOTO"/>
     </div>
-    <div>
-
+    <div class="flex text-white bg-white h-10 items-center rounded-3xl flex-between px-1 py-1 space-x-2">
+      <div>
+        <img src="@/assets/flag/fr.png" alt="" class="object-cover w-6 h-6 rounded-full ">
+      </div>
+      <div>
+        <select class="text-black w-12" v-model="lang">
+          <option value="fr">FR</option>
+          <option value="en">EN</option>
+        </select>
+      </div>
+      <div class="bg-[#FE385C] rounded-2xl h-full cursor-pointer text-[14px] flex items-center justify-center w-20 hover:bg-[#FE637E] active:bg-[#FC002D] transition duration-150 select-none ">
+        SingUp
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +30,7 @@
 
 import TopbarLink from "~/components/molecules/TopbarLink.vue";
 
+const lang = ref("")
 
 </script>
 
