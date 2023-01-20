de8501da78a0fddf94bb967a5256fbc9891f8138
rmingon <ronanmingon69550@gmail.com>
2023-01-20 08:02:33 +0100

    add tailwind color config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,3 +1,5 @@
+
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,6 +13,14 @@ module.exports = {
   theme: {
     colors: {
       'midnight': '#090723',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
     },
     extend: {},
   },
