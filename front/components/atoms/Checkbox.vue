ee389dff1cd157845f94f06d8e05cd8eed4a6dd6
Ronan <ronanmingon69550@gmail.com>
2023-01-21 22:16:27 +0100

    add checkbox

---
+++ front/components/atoms/Checkbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex items-center pl-3">
+    <input id="vue-checkbox-list" v-model="props.checked" @click="emit('changed')" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+    <label for="vue-checkbox-list" class="w-full py-1 ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">{{props.title}}</label>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const emit = defineEmits(['changed'])
+  const props = defineProps<{
+    title: string,
+    checked: boolean
+  }>()
+</script>
