154f23cb3b9d1d8446d3b4763b79d994f3ee1efa
Ronan <ronanmingon69550@gmail.com>
2023-01-21 22:45:10 +0100

    fix Checkbox

--- front/components/atoms/Checkbox.vue
+++ front/components/atoms/Checkbox.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="flex items-center pl-3">
-    <input id="vue-checkbox-list" v-model="props.checked" @click="emit('changed')" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-    <label for="vue-checkbox-list" class="w-full py-1 ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">{{props.title}}</label>
+    <input :id="title" v-model="checked" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+    <label :for="title" class="w-full py-1 ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">{{props.title}}</label>
   </div>
 </template>
 
 <script setup lang="ts">
+  import {computed} from "@vue/reactivity";
+
   const emit = defineEmits(['changed'])
   const props = defineProps<{
     title: string,
     checked: boolean
   }>()
+
+  const checked = computed({
+    get() {return props.checked},
+    set(checked) {
+      emit('changed', checked)
+    }
+  })
 </script>
