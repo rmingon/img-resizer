3b4306da9f911d78367b413b2024a7b53f604525
Ronan <ronanmingon69550@gmail.com>
2023-01-22 18:35:37 +0100

    working file upload

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { FilesModule } from './files/files.module';
+import {MulterModule} from "@nestjs/platform-express";
+import { SharpModule } from './sharp/sharp.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot(), PrismaModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    PrismaModule,
+    FilesModule,
+    MulterModule.register({
+      dest: './pictures',
+    }),
+    SharpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/files/files.controller.ts
+++ back/src/files/files.controller.ts
@@ -1,13 +1,22 @@
 import {Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from "multer";
+import {randomUUID} from "crypto";
 
 @Controller('files')
 export class FilesController {
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() files: Array<Express.Multer.File>) {
-    console.log(files);
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './pictures',
+      filename: (req, file, cb) => {
+        cb(null, `${randomUUID()}_${file.originalname}`)
+      }
+    })
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 
 }
