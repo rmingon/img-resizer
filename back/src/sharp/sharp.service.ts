ed698a8619280684fb9fec2c26f50b498c590f9c
Ronan <ronanmingon69550@gmail.com>
2023-01-24 06:20:23 +0100

    add sharp method

--- back/src/sharp/sharp.service.ts
+++ back/src/sharp/sharp.service.ts
@@ -10,5 +10,11 @@ type Format = "png" | "jpeg"
 
 @Injectable()
 export class SharpService {
-
+  resize(img_location: string, {width, height}: Size, format: Format) {
+    sharp(img_location)
+      .resize(width, height)
+      .toFile('output.webp', (err, info) => {
+        throw err
+      });
+  }
 }
